a98f0fd8ca945d9b2ae56871d65657df369f3ee9
Chandan Kumar <chandankumarsingh1351@gmail.com>
2021-01-17 00:55:17 +0530

    End Of Section 3

--- DatingApp/Controllers/UsersController.cs
+++ DatingApp/Controllers/UsersController.cs
@@ -27,9 +27,9 @@ namespace API.Controllers
 
         //api/users/3
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<AppUser> GetUser(int id)
         {
-            return _context.Users.Find(id);
+            return await _context.Users.FindAsync(id);
         }
     }
 }
